8b07ff9f46d16d7ad3faca442d94b40ac0e42b59
Oscar Barajas <oscar@arepa.dev>
2020-06-23 12:48:47 -0500

    [10] blogpost

---
+++ src/components/Post.svelte
@@ -0,0 +1,68 @@
+<script>
+  export let post;
+</script>
+
+<style>
+  .Post-item {
+    color: #191a22;
+    position: relative;
+  }
+  .Post-content {
+    font-size: 16px;
+    font-weight: 300;
+    display: grid;
+    justify-content: space-between;
+    grid-gap: 5px;
+    grid-template-columns: 1fr;
+  }
+  .Post-title {
+    font-size: 20px;
+    margin: 0;
+    padding: 0;
+  }
+  .Post-title p {
+    color: #333;
+    font-size: 13px;
+    font-weight: 300;
+    margin-top: 5px;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: #22215b;
+  }
+
+  .Post-desc p {
+    color: #333;
+    font-size: 16px;
+    line-height: 28px;
+    margin: 0 auto;
+    word-break: break-word;
+  }
+
+  .dot {
+    font-weight: 700;
+  }
+</style>
+
+<div class="Post-item">
+  <div class="Post-content">
+    <div class="Post-head">
+      <div class="Post-title">
+        <h2>
+          <a rel="prefetch" href="blog/{post.slug}">{post.title}</a>
+        </h2>
+        <p>
+          <time datetime={post.createdAt}>{post.createdAT}</time>
+          <span class="dot">.</span>
+          <span>5 min</span>
+        </p>
+      </div>
+      <div class="Post-tags" />
+      <div class="Post-desc">
+        <p>{post.desc}</p>
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,41 +1,35 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`blog.json`)
+      .then(r => r.json())
+      .then(posts => {
+        return { posts };
+      });
+  }
+</script>
+
+<script>
+  import Post from "../components/Post.svelte";
+  export let posts;
+</script>
+
 <style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
+  .Posts {
+    display: grid;
+    justify-content: space-between;
+    grid-gap: 15px;
+    grid-template-columns: 1fr;
   }
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Oscar Barajas Tavares</title>
 </svelte:head>
 
-<div class="Home">Blogposts</div>
+<div class="Home">
+  <div class="Posts">
+    {#each posts as post}
+      <Post {post} />
+    {/each}
+  </div>
+</div>
