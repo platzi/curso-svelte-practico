70b52fb789e4d9f9716071429a519d0345ec2fa8
Oscar Barajas <oscar@arepa.dev>
2020-06-23 10:48:21 -0500

    [04] about route

---
+++ src/routes/contact.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>Contact</title>
+</svelte:head>
+
+<div class="Contact">
+  <h2>Contact</h2>
+  <p>Your contact informacion</p>
+</div>
---
+++ src/routes/general.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>General</title>
+</svelte:head>
+
+<div class="General">
+  <h2>General</h2>
+</div>
