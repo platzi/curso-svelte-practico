30dfc725ef626aa19b7a4ab821fa12e5b1777955
Oscar Barajas <oscar@arepa.dev>
2020-06-23 11:34:18 -0500

    [06] Nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,10 +3,23 @@
 </script>
 
 <style>
-  nav {
-    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+  .Nav {
+    border-bottom: 1px solid rgba(255, 0, 121, 0.2);
     font-weight: 300;
-    padding: 0 1em;
+    background-color: #fafbfc;
+    color: #191a22;
+  }
+
+  .Nav-container {
+    display: grid;
+    grid-template-columns: minmax(auto, 1024px);
+    justify-content: center;
+    align-items: center;
+  }
+
+  .Nav-content {
+    display: flex;
+    justify-content: space-between;
   }
 
   ul {
@@ -14,7 +27,6 @@
     padding: 0;
   }
 
-  /* clearfix */
   ul::after {
     content: "";
     display: block;
@@ -43,44 +55,61 @@
 
   a {
     text-decoration: none;
-    padding: 1em 0.5em;
+    padding: 1em 1em 1em 0;
     display: block;
+    font-size: 14px;
+    font-weight: 300;
+    transition: all 0.3s ease-in-out 0s;
+    color: rgba(25, 26, 34, 0.7);
+  }
+  a:hover {
+    color: rgba(25, 26, 34, 1);
   }
 </style>
 
-<nav>
-  <ul>
-    <li>
-      <a aria-current={segment === undefined ? 'page' : undefined} href=".">
-        home
-      </a>
-    </li>
-    <li>
-      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
-        about
-      </a>
-    </li>
-    <li>
-      <a
-        aria-current={segment === 'contact' ? 'page' : undefined}
-        href="contact">
-        contact
-      </a>
-    </li>
-    <li>
-      <a
-        aria-current={segment === 'general' ? 'page' : undefined}
-        href="general">
-        general
-      </a>
-    </li>
-    <li>
-      <a
-        rel="prefetch"
-        aria-current={segment === 'blog' ? 'page' : undefined}
-        href="blog">
-        blog
-      </a>
-    </li>
-  </ul>
+<nav class="Nav">
+  <div class="Nav-container">
+    <div class="Nav-content">
+      <div class="Nav-social">
+        <ul>
+          <li>
+            <a
+              aria-current={segment === undefined ? 'page' : undefined}
+              href=".">
+              home
+            </a>
+          </li>
+          <li>
+            <a
+              aria-current={segment === 'about' ? 'page' : undefined}
+              href="about">
+              about
+            </a>
+          </li>
+          <li>
+            <a
+              aria-current={segment === 'contact' ? 'page' : undefined}
+              href="contact">
+              contact
+            </a>
+          </li>
+          <li>
+            <a
+              aria-current={segment === 'general' ? 'page' : undefined}
+              href="general">
+              general
+            </a>
+          </li>
+          <li>
+            <a
+              rel="prefetch"
+              aria-current={segment === 'blog' ? 'page' : undefined}
+              href="blog">
+              blog
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </nav>
