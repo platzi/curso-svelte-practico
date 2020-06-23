594d027ec0632189cd258d1fbd193b82a7cc1836
Oscar Barajas <oscar@arepa.dev>
2020-06-23 12:34:27 -0500

    [09] post dummy

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -3,7 +3,13 @@ import posts from './_posts.js';
 const contents = JSON.stringify(posts.map(post => {
 	return {
 		title: post.title,
-		slug: post.slug
+		slug: post.slug,
+		desc: post.desc,
+		createdAt: post.createdAt,
+		html: post.html,
+		tag: post.tag,
+		image: post.imagen,
+		id: post.id,
 	};
 }));
 
