71432483c761951389b3ec13853651084e4a9147
Oscar Barajas <oscar@arepa.dev>
2020-06-24 10:05:09 -0500

    [15] slug

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,64 +1,57 @@
 <script context="module">
-	export async function preload({ params, query }) {
-		// the `slug` parameter is available because
-		// this file is called [slug].svelte
-		const res = await this.fetch(`blog/${params.slug}.json`);
-		const data = await res.json();
+  export async function preload({ params, query }) {
+    // the `slug` parameter is available because
+    // this file is called [slug].svelte
+    const res = await this.fetch(`blog/${params.slug}.json`);
+    const data = await res.json();
 
-		if (res.status === 200) {
-			return { post: data };
-		} else {
-			this.error(res.status, data.message);
-		}
-	}
+    if (res.status === 200) {
+      return { post: data };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
 </script>
 
 <script>
-	export let post;
+  import readingTime from "../../utils/readingTime";
+  import formatIsoTime from "../../utils/formatIsoTime";
+  export let post;
 </script>
 
 <style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
-
-	.content :global(pre) {
-		background-color: #f9f9f9;
-		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
-
-	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
-	}
-
-	.content :global(ul) {
-		line-height: 1.5;
-	}
-
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
-	}
+  h2 {
+    color: #22215b;
+    font-size: 28px;
+    margin: 0;
+    padding: 0;
+  }
+  .Post-title p {
+    color: #555;
+    font-size: 14px;
+    font-weight: 300;
+    margin-top: 5px;
+    padding: 0;
+  }
+  .comments {
+    margin: 2em 0 0 0;
+  }
 </style>
 
 <svelte:head>
-	<title>{post.title}</title>
+  <title>{post.title}</title>
 </svelte:head>
 
-<h1>{post.title}</h1>
-
-<div class='content'>
-	{@html post.html}
+<div class="Post">
+  <div class="Post-title">
+    <h2>{post.title}</h2>
+  </div>
+  <p class="date">
+    <time datatime={post.createdAt}>📅 {formatIsoTime(post.createdAt)}</time>
+    <span>{readingTime(post.html)}</span>
+  </p>
+  <div class="content">
+    {@html post.html}
+  </div>
+  <div class="comments" />
 </div>
