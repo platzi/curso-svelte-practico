316d5eafa8e174e2c1e6312904fde057e5912dd7
Oscar Barajas <oscar@arepa.dev>
2020-06-26 09:48:20 -0500

    [19] slugs/blog

--- src/routes/general.svelte
+++ src/routes/general.svelte
@@ -1,7 +1,41 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`blog.json`)
+      .then(r => r.json())
+      .then(posts => {
+        return { posts };
+      });
+  }
+</script>
+
+<script>
+  import Post from "../components/Post.svelte";
+  export let posts;
+  const filterPost = posts.filter(post => post.tag === "general");
+</script>
+
+<style>
+  .Posts {
+    display: grid;
+    justify-content: space-between;
+    grid-gap: 30px;
+    grid-template-columns: 1fr;
+  }
+</style>
+
 <svelte:head>
   <title>General</title>
 </svelte:head>
 
 <div class="General">
-  <h2>General</h2>
+  <h1>General</h1>
+  <div class="Posts">
+    {#if filterPost.length >= 1}
+      {#each filterPost as post}
+        <Post {post} />
+      {/each}
+    {:else}
+      <p>Sin resultados...</p>
+    {/if}
+  </div>
 </div>
