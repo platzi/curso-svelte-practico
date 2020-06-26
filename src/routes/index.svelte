09822ed5caa4adec7dcc15c1a902f067927f3c64
Oscar Barajas <oscar@arepa.dev>
2020-06-26 11:12:07 -0500

    SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,6 +24,28 @@
 
 <svelte:head>
   <title>Oscar Barajas Tavares</title>
+  <meta name="description" content="Blog de gndx live" />
+
+  <meta name="twitter:card" content="summary_larga_image" />
+  <meta name="twitter:site" content="@gndx" />
+  <meta name="twitter:creator" content="@gndx" />
+  <meta name="twitter:title" content="Oscar Barajas Tavares" />
+  <meta name="twitter:descripcion" content="Blog de gndx live" />
+  <meta
+    name="twitter:image"
+    content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+
+  <meta name="og:title" content="Oscar Barajas Tavares" />
+  <meta name="og:description" content="Blog de gndx live" />
+  <meta
+    name="og:image"
+    content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+  <meta name="og:url" content="https://blog.gndx.live" />
+  <meta name="og:site_name" content="Oscar Barajas Tavares" />
+  <meta name="og:locale" content="es_ES" />
+  <meta name="og:type" content="article" />
+  <meta name="fb:app_id" content="" />
+
 </svelte:head>
 
 <div class="Home">
